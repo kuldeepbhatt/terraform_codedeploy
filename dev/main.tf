61b191c740a95cbf4788c2e605349a77ec90e1e1
kuldeep.b <kuldeep.bhatt@zymr.com>
2019-05-02 15:28:07 +0530

    Unused files and code base removed

--- dev/main.tf
+++ dev/main.tf
@@ -2,28 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "kd_vpc" {
-  source = "../modules/vpc"
-  vpc_cidr = "54.172.60.0/23"
-  vpc_tenancy = "default"
-  vpc_id="${module.kd_vpc.vpc_id}"
-  subnet_cidr="54.172.60.0/27"
-}
-# module "kd_ec2" {
-#   source = "../modules/ec2"
-#   count = 1
-#   ami_id = "0080e4c5bc078760e"
-#   instanceType = "t2.micro"
-#   subnet_id = "${module.kd_vpc.subnet_id}"
-# }
 module "kd_asg" {
   source = "../modules/asg"
   asgname="kd_autoscalinggroup"
   min_scaling_to = 1
   max_scaling_to = 2
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  # vpc_zone_identifier = ["${module.kd_vpc.subnet_id}"]
-  # sec_group_vpc_id = "${module.kd_vpc.vpc_id}"
   config_name = "kd_lc"
   tags_name = "kd_instance1"
   image_id = "ami-0080e4c5bc078760e"
--- modules/asg/asg.tf
+++ modules/asg/asg.tf
@@ -54,32 +54,11 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
   role = "${aws_iam_role.s3_access_role.name}"
 }
 
-## Security Group for ELB
-resource "aws_security_group" "kd_sec_group" {
-  name = "${var.sec_group_name}"
-  # vpc_id      = "${var.sec_group_vpc_id}"
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    name = "kd_sec_group"
-  }
-}
 ## Creating Launch Configuration
 resource "aws_launch_configuration" "kd_launch_config" {
   name          = "${var.config_name}"
   image_id      = "${var.image_id}"
   instance_type = "${var.instanceType}"
-  # security_groups = ["${aws_security_group.kd_sec_group.id}"]
   user_data = "${data.template_file.user_data.rendered}"
   iam_instance_profile = "${aws_iam_instance_profile.s3_access_profile.name}"
   
@@ -87,24 +66,6 @@ resource "aws_launch_configuration" "kd_launch_config" {
     create_before_destroy = true
   }
 }
-# resource "aws_elb" "kd-elb" {
-#   name = "${var.elbname}"
-#   security_groups = ["${aws_security_group.kd_sec_group.id}"]
-#   availability_zones = "${var.availability_zones}"
-#   health_check {
-#     healthy_threshold = 2
-#     unhealthy_threshold = 2
-#     timeout = 3
-#     interval = 30
-#     target = "HTTP:8080/"
-#   }
-#   listener {
-#     lb_port = 80
-#     lb_protocol = "http"
-#     instance_port = "8080"
-#     instance_protocol = "http"
-#   }
-# }
 
 resource "aws_autoscaling_group" "kd_asg" {
   name                 = "${var.asgname}"
@@ -112,9 +73,6 @@ resource "aws_autoscaling_group" "kd_asg" {
   min_size             = "${var.min_scaling_to}"
   max_size             = "${var.max_scaling_to}"
   availability_zones = "${var.availability_zones}"
-  # vpc_zone_identifier = ["${var.vpc_zone_identifier}"]
-  
-  # load_balancers = ["${aws_elb.kd-elb.name}"]
   tag {
    key                 = "Name"
    value               = "${var.tags_name}"
